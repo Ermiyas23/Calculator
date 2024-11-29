02da22de9f6be775636f57ac09d07dae4ccd3707
Ermiyas23 <ermi23@on.com>
2024-11-29 05:44:45 -0800

    Operate work is done

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ let multiply = function( a , b){
 let division = function(a , b){
     return a / b;
 };
+
+let operate = function(operator,num1,num2){
+    switch (operator) {
+        case "+":
+            return add(num1 , num2);
+        case '-':
+            return subtract(num1 , num2);
+        case '*':
+            return multiply(num1 , num2);
+        case '/':
+            return division( num1 , num2);
+        default:
+            break;
+    }
+}
+
+let show = operate('*',8 ,8);
+console.log(show)
